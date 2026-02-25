0d9689e818d24f00fe3fbed2726805d8663da90f
Rafi Arrafif <rafiarrafif05@gmail.com>
2026-02-25 20:25:53 +0700

    🚧 wip: add dummy example route

--- modules/kuronime/kuronime.controller.ts
+++ modules/kuronime/kuronime.controller.ts
@@ -5,19 +5,54 @@ import { KuronimeRepository } from "./kuronime.repository";
 
 export class KuronimeController {
   async mainHandler() {
-    const slug = await this.getSlugUrl();
+    try {
+      const { action } = await inquirer.prompt([
+        {
+          type: "select",
+          name: "action",
+          message: "Select scrapping mode:",
+          choices: [
+            { name: "Per Episode", value: "perEpisode" },
+            { name: "All Episode (Batch)", value: "allEpisode" },
+          ],
+        },
+      ]);
+
+      switch (action) {
+        case "perEpisode":
+          await this.perEpisodeHandler();
+          break;
+        case "allEpisode":
+          await this.allEpisodeHandler();
+          break;
+        default:
+          console.log("Invalid action selected.");
+      }
+    } catch {
+      console.error("An error occurred during the scraping process.");
+    }
+  }
+
+  async perEpisodeHandler() {
+    const slug = await this.getSlugUrl("nonton-one-piece-episode-1");
     const data = await new KuronimeService().scrapeAnimeData(slug);
     await new KuronimeRepository().saveToJson(data, slug);
     await closeBrowser();
   }
 
-  async getSlugUrl() {
+  async allEpisodeHandler() {
+    const slug = await this.getSlugUrl("anime/one-piece");
+    const data = await new KuronimeService().scrapeAnimeData(slug);
+    await new KuronimeRepository().saveToJson(data, slug);
+    await closeBrowser();
+  }
+
+  async getSlugUrl(example: string) {
     const input = await inquirer.prompt([
       {
         type: "input",
         name: "slug",
-        message:
-          "Enter the slug of the anime you want to scrape (e.g. 'one-piece-1000'):",
+        message: `Enter the slug of the anime you want to scrape (e.g. \`${example}\`):`,
         validate: function (input) {
           if (input.trim() === "") {
             return "Slug cannot be empty. Please enter a valid slug.";
